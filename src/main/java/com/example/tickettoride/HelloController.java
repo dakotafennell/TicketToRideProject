5bb7669a5007b45e19d64cb208600ba325655e12
Louis Fennell III <louistfennell93@gmail.com>
2023-10-04 21:45:42 -0400

    Initial updates

---
+++ src/main/java/com/example/tickettoride/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.tickettoride;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to Ticket to Ride New York!");
+    }
+}
