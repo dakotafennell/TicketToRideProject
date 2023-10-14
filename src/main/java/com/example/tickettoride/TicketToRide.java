5952265513234442bc8fa59649000a9ecb4473c0
JLemois <147009876+JLemois@users.noreply.github.com>
2023-10-14 17:32:33 -0400

    Imported inserts and labels, changed from blank start to general plan labelled out in corresponding areas. -**Consider moving the inline class, that was made for initial viewing**-
    --By: J Lemois

--- src/main/java/com/example/tickettoride/TicketToRide.java
+++ src/main/java/com/example/tickettoride/TicketToRide.java
@@ -3,6 +3,13 @@ package com.example.tickettoride;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,16 +20,48 @@ import java.io.IOException;
  */
 public class TicketToRide extends Application
 {
+
+    //Border and Stack panes setup with similar naming conventions ~JCL
+    BorderPane borderPane;
+    StackPane stackPane;
+
+    Canvas canvas;
+
     @Override
     public void start(Stage stage) throws IOException
     {
+
+        StackPane stackPane = new StackPane();
+        BorderPane borderPane = new BorderPane();
+
+        //setup of borderPane displays titles
+        borderPane.setTop(new LabelPane("By: Austin, Joseph, and Louis!"));
+        borderPane.setRight(new LabelPane("Draw & Discard area!"));
+        borderPane.setBottom(new LabelPane("Displays current players hand!"));
+        borderPane.setLeft(new LabelPane("Displays players in turn order with points!"));
+
+
+
         FXMLLoader fxmlLoader = new FXMLLoader(TicketToRide.class.getResource("hello-view.fxml"));
         //Scene set to width of a 720p screen
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        //Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+
+        //set scene to borderpane with 720p screen size
+        Scene scene = new Scene(borderPane, 1280, 720);
+
         //Title of Game
         stage.setTitle("Ticket to Ride");
         stage.setScene(scene);
         stage.show();
+    }
+
+    //sets up border and allows text to be displayed
+    class LabelPane extends StackPane {
+        public LabelPane(String title) {
+            getChildren().add(new Label(title));
+            setStyle("-fx-border-color: blue");
+            setPadding(new Insets(35, 35,35,35));
+        }
     }
 
     public static void main(String[] args)
