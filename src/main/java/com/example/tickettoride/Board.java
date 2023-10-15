b32bd0b562066091d139613b5c42f46673294c06
Austinb238 <147009922+Austinb238@users.noreply.github.com>
2023-10-15 19:47:56 -0400

    Update Board.java

--- src/main/java/com/example/tickettoride/Board.java
+++ src/main/java/com/example/tickettoride/Board.java
@@ -1,11 +1,29 @@
 package com.example.tickettoride;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import java.io.File;
 
 /**
  * Board class
  * This class will contain logic for displaying and/or updating the board and map of the game
  *
  */
-public class Board
-{
-    //Board display logic
+public class Board extends StackPane {
+    // Board display logic
+    public ImageView getTicketToRideImage() {
+        ImageView ticketToRideImageView = new ImageView();
+        try {
+            // Load the image from a file
+            File imageFile = new File("resources/TicketToRide.png");
+            String imageUrl = imageFile.toURI().toURL().toString();
+            Image ticketToRideImage = new Image(imageUrl);
+
+            // Set the image in the ImageView
+            ticketToRideImageView.setImage(ticketToRideImage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ticketToRideImageView;
+    }
 }
