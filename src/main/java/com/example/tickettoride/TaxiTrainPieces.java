0b6b3c33d72057ade5d0f1461dd7802480132eb9
Louis Fennell <louistfennell93@gmail.com>
2023-10-19 23:05:30 -0400

    updated pieces file

--- src/main/java/com/example/tickettoride/TaxiTrainPieces.java
+++ src/main/java/com/example/tickettoride/TaxiTrainPieces.java
@@ -7,6 +7,15 @@ package com.example.tickettoride;
  */
 public class TaxiTrainPieces
 {
+
+    //Contains the maximum number of train pieces per player
+    private int maxNumTrainPieces = 45;
+    private int minNumTrainPieces = 3;
+
+    //Contains the maximum number of taxi pieces per player
+    private int maxNumTaxiPieces = 15;
+    private int minNumTaxiPieces = 2;
+
     //Variable will be used to determine which type of piece
     private String vehicleType;
     //Variable to contain the piece's color
